ce54b0a85ee530afc47bc9764c61e15b09aa17e4
noagedo <gedonoa@gmail.com>
2025-03-07 15:43:42 +0200

    edit profile

--- app/src/main/java/com/example/neighborhub/ui/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/example/neighborhub/ui/viewmodel/PostViewModel.kt
@@ -1,54 +1,3 @@
-//package com.example.neighborhub.ui.viewmodel
-//
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.ViewModel
-//import androidx.lifecycle.viewModelScope
-//import com.example.neighborhub.repository.PostRepository
-//import com.example.neighborhub.model.Post
-//import kotlinx.coroutines.launch
-//
-//class PostViewModel : ViewModel() {
-//    private val repository = PostRepository()
-//
-//    private val _allPosts = mutableListOf<Post>() // Store all posts here
-//
-//    private val _posts = MutableLiveData<List<Post>>()
-//    val posts: LiveData<List<Post>> = _posts
-//
-//    private val _isLoading = MutableLiveData(false)
-//    val isLoading: LiveData<Boolean> = _isLoading
-//
-//    private val _errorMessage = MutableLiveData<String?>(null)
-//    val errorMessage: LiveData<String?> = _errorMessage
-//
-//    fun fetchPosts() {
-//        _isLoading.value = true
-//        viewModelScope.launch {
-//            val result = repository.getAllPosts()
-//            if (result.isNotEmpty()) {
-//                _allPosts.clear()
-//                _allPosts.addAll(result) // Cache the original list
-//                _posts.value = _allPosts
-//                _errorMessage.value = null
-//            } else {
-//                _errorMessage.value = "No posts found"
-//            }
-//            _isLoading.value = false
-//        }
-//    }
-//
-//    fun searchPosts(query: String) {
-//        if (query.isBlank()) {
-//            _posts.value = _allPosts // Reset to the original list
-//        } else {
-//            _posts.value = _allPosts.filter { post ->
-//                post.headline.contains(query, true) || post.userName.contains(query, true)
-//            }
-//        }
-//    }
-//
-//}
 package com.example.neighborhub.ui.viewmodel
 
 import android.content.Context
@@ -82,7 +31,6 @@ class PostViewModel(context: Context) : ViewModel() {
                     _allPosts.clear()
                     _allPosts.addAll(result)
                     _posts.value = _allPosts
-                    _posts.value = result
                     _errorMessage.value = null
                 } else {
                     _errorMessage.value = "No posts found"
